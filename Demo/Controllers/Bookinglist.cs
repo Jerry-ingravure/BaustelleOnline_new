8b113bdee51edeb84fca134e4b9240edeed0d4f4
Jerry-ingravure <107117130+Jerry-ingravure@users.noreply.github.com>
2022-06-14 13:58:57 +0200

    Version1.0

---
+++ Demo/Controllers/Bookinglist.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class ViewData : Controller
+    {
+        public IActionResult Booking()
+        {
+
+            List<avise> avises = new List<avise>();
+            avise.Add(new avise(0, "xyz", "eg1", "offen"));
+            avise.Add(new avise(1, "xy", "eg2", "offen"));
+            avise.Add(new avise(2, "x", "eg3", "offen"));
+
+
+            return View("Booking", avises);
+        }
+    }
+}
+
---
+++ Demo/Models/avise.cs
@@ -0,0 +1,31 @@
+﻿namespace Demo.Models
+{
+    public class avise
+    {
+        public int Id { get; set; }
+        public string Besteller { get; set; }
+        public string Entladestellen { get; set; }
+        public string status { get; set; }
+
+        public avise()
+        {
+            Id = 0;
+            Besteller = "Demomandant";
+            Entladestellen = "Bauteil 1";
+            status = "offen";
+        }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public avise(int v1, string v2, string v3, string v4)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+        }
+
+        internal static void Add(avise avise)
+        {
+            
+        }
+    }
+
+    
+    }
